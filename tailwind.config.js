70670ee2e4f87b75ef0dfc766a1c4935f1d23e51
Andres Soria <andres.zov@gmail.com>
2023-06-08 18:10:54 -0600

    Customize theme colors, fonts and breakpoints

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,28 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ['./src/**/*.{html,js}'],
+  content: ['./public/index.html', './src/**/*.{html,js}'],
   theme: {
-    extend: {},
+    fontFamily: {
+      Poppins: ['Poppins, sans-serif'],
+    },
+    container: {
+      center: true,
+      padding: '1rem',
+      screens: {
+        lg: '1124px',
+        xg: '1124px',
+        '2xl': '1124px',
+      },
+    },
+    extend: {
+      colors: {
+        'bookmark-purple': '#5267DF',
+        'bookmark-red': '#FA5959',
+        'bookmark-blue': '#242A45',
+        'bookmark-grey': '#9194A2',
+        'bookmark-white': '#f7f7f7',
+      },
+    },
   },
   plugins: [],
 };
